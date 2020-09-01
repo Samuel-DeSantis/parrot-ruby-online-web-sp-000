3945c13414da6203a338c54b183f1c93f4584b23
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-09-01 05:07:07 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,3 +1,10 @@
 def parrot(input = "Squawk!")
+  if (input === "Squawk!")
+    puts "Squawk!"
+    return "Squawk!"
+  else
+    puts input
+    return input
+  end
   (input === "Squawk!") ? (return "Squawk!") : (return input)
 end
